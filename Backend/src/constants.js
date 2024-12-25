2307c5a396eb09fc45f4c6ee29aea74d89accb2e
S672672 <bsmith672@rku.ac.in>
2024-12-25 13:45:42 +0545

    setup done

---
+++ Backend/src/constants.js
@@ -0,0 +1,3 @@
+// const DB_NAME = 'playtube'
+
+// export default DB_NAME
---
+++ Backend/src/db/config.js
@@ -0,0 +1,13 @@
+// import dotenv from 'dotenv'
+// dotenv.config();
+
+// const config = ({
+//   PORT: process.env.PORT || 5001,
+//   MONGO_URI: process.env.MONGO_URI,
+//   JWT_SECRET: process.env.JWT_SECRET,
+//   ADMIN_EMAIL: process.env.ADMIN_EMAIL,
+//   ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
+// });
+// console.log(process.env.PORT)
+
+// export default config
