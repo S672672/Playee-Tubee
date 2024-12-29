00b7d710e599e18f67bef4e69131f3e1bb6c77cc
S672672 <bsmith672@rku.ac.in>
2024-12-29 18:05:29 +0545

    undefined issue solved

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -31,8 +31,14 @@ const registerUser = asyncHandler(async (req, res) => {
   if(existedUser){
     throw new ApiError(409,'User with email or username already exists')
   }
+  console.log(req.files)
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImagePath = req.files?.coverImage[0]?.path;
+  // const coverImagePath = req.files?.coverImage[0]?.path;
+
+  let coverImagePath;
+  if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+  coverImagePath = req.files.coverImage[0].path
+  }
 
   if(!avatarLocalPath){
     throw new ApiError(400,'Avatar file is required')
